c6766d994a3a482b52059036b01d05e258e70f25
Ankit Ghildiyal <cosmothecreater@gmail.com>
2020-04-23 04:52:39 +0530

    Blog functionality and polish

--- blog/models.py
+++ blog/models.py
@@ -7,3 +7,12 @@ class Blog(models.Model):
     pub_date = models.DateTimeField(default=datetime.now())
     body=models.TextField(default='00000')
     image =models.ImageField(upload_to='images/',default='/media/images/index.jpeg')
+
+    def __str__(self):
+        return self.title
+    
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime("%b %e %Y")
